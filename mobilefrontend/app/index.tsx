1bc20389dd6b0ec2bd7284289ea7ff6b9ad0a369
AustinCLieu <asianonrice@gmail.com>
2026-03-24 20:05:22 -0700

    Added mobielfrotnend new expo app

---
+++ mobilefrontend/app/index.tsx
@@ -0,0 +1,28 @@
+/*
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+*/
+
+import { router } from 'expo-router';
+import { Button, View } from 'react-native';
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Button title="Test Supabase" onPress={() => router.push('/supabaseConnection')} />
+    </View>
+  );
+}
